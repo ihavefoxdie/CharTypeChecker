b57a91519709bf09f2ce30dd0416fda9ea6b5b97
purpeliz <vladislav.nekrasov123@gmail.com>
2022-10-12 15:18:41 +0300

    Add project files.

---
+++ Types of chars/Classes/CharType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ExtentionMethods
+{
+    public static class CharType
+    {
+        public static string Check(this string text)
+        {
+            if (Regex.IsMatch(text, @"^[a-zA-Z]"))
+                return "Letters";
+
+            if (Regex.IsMatch(text, @"^[0-9]"))
+                return "Digits";
+
+            if (Regex.IsMatch(text, @"^[\-\.\,\!\?\;\:]"))
+                return "Punctuation";
+
+            if (Regex.IsMatch(text, @"^[\+\-\*\/\%\>\<\=]"))
+                return "Arithmetic";
+
+            return "Nothing";
+        }
+    }
+}
---
+++ Types of chars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExtentionMethods;
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string test = ",";
+
+            //Console.WriteLine(test.Check());
+            Console.WriteLine(Console.ReadLine().Check());
+
+        }
+    }
+}
